c2d607dd2c68f4322d42e99bc5f41f2f834f4caa
pmarques <pmarques@idealista.com>
2020-03-21 01:37:03 +0100

    Added total amount test

--- src/tests/ToxicForm.test.js
+++ src/tests/ToxicForm.test.js
@@ -2,7 +2,31 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import ToxicForm from '../components/ToxicForm';
 
-test('Clicking submit, all added styles are removed', ()=>{
+test('After clicking both in a user and in an amount button, the submit button is enabled', ()=>{
+    const { container } = render(<ToxicForm />);
+    const firstUserInput = container.querySelectorAll('.users div')[0].children[0];
+    const firstMoneyBtn = container.querySelector('.money button');
+    const submitBtn = container.querySelector('#sendButton');
+    expect(submitBtn.disabled).toBe(true);
+    fireEvent.click(firstUserInput);
+    expect(submitBtn.disabled).toBe(true);
+    fireEvent.click(firstMoneyBtn);
+    expect(submitBtn.disabled).toBe(false);
+});
+
+test('After clicking both in an amount button and then in a user, the submit button is enabled', ()=>{
+    const { container } = render(<ToxicForm />);
+    const firstUserInput = container.querySelectorAll('.users div')[0].children[0];
+    const firstMoneyBtn = container.querySelector('.money button');
+    const submitBtn = container.querySelector('#sendButton');
+    expect(submitBtn.disabled).toBe(true);
+    fireEvent.click(firstMoneyBtn);
+    expect(submitBtn.disabled).toBe(true);
+    fireEvent.click(firstUserInput);
+    expect(submitBtn.disabled).toBe(false);
+});
+
+test('After clicking submit, all added styles from clicking form items are removed and sendbutton is disabled', ()=>{
     const { container } = render(<ToxicForm />);
     const firstUser = container.querySelectorAll('.users div')[0];
     const firstUserInput =firstUser.children[0];
@@ -11,11 +35,24 @@ test('Clicking submit, all added styles are removed', ()=>{
     fireEvent.click(firstUserInput);
     fireEvent.click(firstMoneyBtn);
     fireEvent.click(submitBtn);
+    expect(submitBtn.disabled).toBe(true);
     firstUser.classList.forEach(item => {
         expect(item).not.toBe('user-selected');
     });
     firstMoneyBtn.classList.forEach(item => {
         expect(item).not.toBe('amount-selected');
     });
-})
+});
 
+test('After adding an amount, we update the Total Amount Value', ()=>{
+    const { container } = render(<ToxicForm />);
+    const firstUserInput = container.querySelectorAll('.users div')[0].children[0];
+    const firstMoneyBtn = container.querySelector('.money button');
+    const submitBtn = container.querySelector('#sendButton');
+    const totalAmountP = container.querySelector('#totalAmount');
+    fireEvent.click(firstUserInput);
+    fireEvent.click(firstMoneyBtn);
+    fireEvent.click(submitBtn);
+    console.log(totalAmountP.innerHTML)
+    expect(totalAmountP.innerHTML).toEqual(expect.stringMatching(Number(firstMoneyBtn.value).toFixed(2)));  
+});
